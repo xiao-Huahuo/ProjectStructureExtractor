82c825ad6bd213fc4da5a16eda6dea3acf47c072
slumpyfufu <1647239706@qq.com>
2025-11-07 13:42:20 +0800

    文件结构重整

--- src/__main__.py
+++
@@ -1,8 +0,0 @@
-from JsonWriter import Writer
-from ProjectStructureTree import TreeBuilder
-from ProjectStructureExtract import Extractor
-import os
-from ProjectStructureGUI import ProjectStructureApp
-if __name__ == '__main__':
-    # ===== 启动应用 =====
-    ProjectStructureApp()
